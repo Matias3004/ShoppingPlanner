b7faceb643f4a2b81c0b638c19a30aeb9ca25e4f
jacek010 <jmyjkowski@gmail.com>
2023-06-07 16:39:56 +0200

    Added some changes to db structure

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   belongs_to :productList
+
+  has_secure_password
+
+  validates :login, presence: true, uniqueness: true, length: {minimum:3, maximum:20}
+  validates :email, presence: true, uniqueness: true
 end
